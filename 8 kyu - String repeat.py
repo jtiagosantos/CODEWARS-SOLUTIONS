e8cff55c703aa62b1220db3401d98c860ef49419
tiago040 <tiagosan040@gmail.com>
2020-08-25 08:51:22 -0300

    Solution to problem 8 kyu - String repeat

---
+++ 8 kyu - String repeat.py
@@ -0,0 +1,10 @@
+'''
+Write a function called repeat_str which repeats the given string src exactly count times.
+
+Example
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+'''
+
+def repeat_str(repeat, string):
+    return string * repeat
