9c848fc44e1dab6997121c97d82d677fea0bc21f
tiago040 <tiagosan040@gmail.com>
2020-08-25 09:10:26 -0300

    Solution to problem 8 kyu - Square (n) Sum

---
+++ 8 kyu - Square(n) Sum.py
@@ -0,0 +1,11 @@
+'''
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+'''
+
+def square_sum(numbers):
+    for k, v in enumerate(numbers):
+        numbers[k] = v ** 2
+    
+    return sum(numbers)
