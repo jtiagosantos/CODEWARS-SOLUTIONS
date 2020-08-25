c002bc84fc4f92f896c4d115e566c1f14267989b
tiago040 <tiagosan040@gmail.com>
2020-08-25 09:04:15 -0300

    Solution to problem 8 kyu - Invert values

---
+++ 8 kyu - Invert values.py
@@ -0,0 +1,13 @@
+'''
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+Example
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+'''
+
+def invert(numbers):
+    for k,v in enumerate(numbers):
+        numbers[k] = v * -1
+    return numbers
