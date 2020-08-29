815c29da951ed2688d1bcf27ad17f51fc0be187c
tiago040 <tiagosan040@gmail.com>
2020-08-29 13:02:44 -0300

    Solution to problem 8 kyu - Abbreviate a Two Word Name

---
+++ 8 kyu - Abbreviate a Two Word Name.py
@@ -0,0 +1,12 @@
+'''
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+Sam Harris => S.H
+Patrick Feeney => P.F
+'''
+
+def abbrev_name(name):
+    return '.'.join([i[0].upper() for i in name.split()])
