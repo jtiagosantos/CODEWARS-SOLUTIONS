cb609527a965bff457813f7539fb8c30f48e1560
tiago040 <tiagosan040@gmail.com>
2020-08-29 13:13:07 -0300

    Solution to problem 6 kyu - Find the unique number

---
+++ 6 kyu - Find the unique number.py
@@ -0,0 +1,16 @@
+'''
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+'''
+
+def find_uniq(array):
+    array2 = array.copy(); array2 = list(set(array2))
+    for i in array2:
+        if array.count(i) > 1:
+            array2.remove(i)
+    return array2[0]
