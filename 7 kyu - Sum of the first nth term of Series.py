a36ecd3f9c60c31b7f64036ff0eb8694991c64f8
tiago040 <tiagosan040@gmail.com>
2020-08-29 09:38:36 -0300

    Solution to problem 7 kyu - Sun of the first nth term of Series

---
+++ 7 kyu - Sum of the first nth term of Series.py
@@ -0,0 +1,24 @@
+'''
+Task:
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:
+SeriesSum(1) => 1 = "1.00"
+SeriesSum(2) => 1 + 1/4 = "1.25"
+SeriesSum(5) => 1 + 1/4 + 1/7 + 1/10 + 1/13 = "1.57"
+'''
+
+def series_sum(n):
+    summation = 0; denominator = 1
+    for _ in range(n):
+        summation += 1 / denominator
+        denominator += 3
+    return str('{:.2f}'.format(summation))
