642a5229f38799b4c075323ef6978479f293c4c5
tiago040 <tiagosan040@gmail.com>
2020-09-03 18:24:21 -0300

    Solution to problem 7 kyu - Is this a triangle

---
+++ 7 kyu - Is this a triangle.py
@@ -0,0 +1,11 @@
+'''
+Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+'''
+
+def is_triangle(a, b, c):
+    sizes = sorted([a,b,c])
+    if sizes[-1] >= sizes[0] + sizes[1]:
+        return False
+    return True
