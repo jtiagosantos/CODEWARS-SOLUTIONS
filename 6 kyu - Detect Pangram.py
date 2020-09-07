8efbb2f5863ae3bf7934b8ef7865eec60e66f2bb
tiago040 <tiagosan040@gmail.com>
2020-09-07 15:44:00 -0300

    Solution to the 6 kyu problem - Detect Pangram

---
+++ 6 kyu - Detect Pangram.py
@@ -0,0 +1,12 @@
+'''
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+'''
+
+def is_pangram(string):
+    alphabet = []; string = list(string.lower())
+    for i in string:
+        if not i in alphabet and i.isalpha():
+            alphabet.append(i)
+    return True if len(alphabet) == 26 else False
